293b3ed8f4b519b440f3415dfe5e2f512389fc84
Kim Jeong Jin <kjjgo35@gmail.com>
2019-11-10 21:36:15 +0900

    NavDrawer - Add Navigation Icons
    Use recycling-view to make navigation icons
    each icons can click and when click turn to other view(activity).
    
    add button contain link to site on Navigation.

--- NavigationDrawer/app/src/main/java/org/example/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/org/example/navigationdrawer/MainActivity.java
@@ -1,8 +1,11 @@
 package org.example.navigationdrawer;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
@@ -17,7 +20,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -46,10 +54,11 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<NavIcon>NavIconArrayList = new ArrayList<>();
         NavIconArrayList.add(new NavIcon(R.drawable.alarm,"알림"));
-        NavIconArrayList.add(new NavIcon(R.drawable.box,"브랜치 공식 작가"));
-        NavIconArrayList.add(new NavIcon(R.drawable.option,"브랜치 하트"));
+        NavIconArrayList.add(new NavIcon(R.drawable.branch,"브랜치 공식 작가"));
+        NavIconArrayList.add(new NavIcon(R.drawable.heart,"브랜치 하트"));
         NavIconArrayList.add(new NavIcon(R.drawable.box,"재료 상자"));
-        NavIconArrayList.add(new NavIcon(R.drawable.question,"재료 스폰서"));
+        NavIconArrayList.add(new NavIcon(R.drawable.dropbox,"작품 만들기"));
+        NavIconArrayList.add(new NavIcon(R.drawable.web,"브랜치 웹"));
         NavIconArrayList.add(new NavIcon(R.drawable.option,"설정"));
         NavIconArrayList.add(new NavIcon(R.drawable.question,"도움말"));
         NavIconArrayList.add(new NavIcon(R.drawable.hand_shake,"제휴제안"));
@@ -60,6 +69,38 @@ public class MainActivity extends AppCompatActivity {
                 this,mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         mDrawerLayout.addDrawerListener(mdrawerToggle);
         mdrawerToggle.syncState();
+
+        Button facebookbutton = findViewById(R.id.gotofacebook);
+        facebookbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://ko-kr.facebook.com/")));
+            }
+        });
+
+        Button youtubebutton = findViewById(R.id.gotoyoutube);
+        youtubebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be")));
+            }
+        });
+
+        Button instagrambutton = findViewById(R.id.gotoinstagram);
+        instagrambutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://instagram.com")));
+            }
+        });
+
+        Button twitterbutton = findViewById(R.id.gototwitter);
+        twitterbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com")));
+            }
+        });
     }
 
     @Override
@@ -86,12 +127,6 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
-
 }
--- NavigationDrawer/app/src/main/java/org/example/navigationdrawer/NavIconAdapter.java
+++ NavigationDrawer/app/src/main/java/org/example/navigationdrawer/NavIconAdapter.java
@@ -1,6 +1,9 @@
 package org.example.navigationdrawer;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,24 +17,62 @@ import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-public class NavIconAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class NavIconAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+{
 
     public static class NavViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         ImageView navIconPic;
         TextView navIconName;
 
+        private final Context context;
+
         public NavViewHolder(View itemView) {
             super(itemView);
+            context = itemView.getContext();
 
-            itemView.setOnClickListener(this);
             navIconPic = itemView.findViewById(R.id.nav_icon_pic);
             navIconName = itemView.findViewById(R.id.nav_icon_name);
+            navIconName.setClickable(true);
+            navIconPic.setClickable(true);
+            navIconName.setOnClickListener(this);
+            navIconPic.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-            Intent intent = new Intent(view.getContext(), AlarmActivity.class);
-            view.getContext().startActivity(intent);
+            final Intent intent;
+            switch(getAdapterPosition()) {
+                case 0:
+                    intent = new Intent(context,AlarmActivity.class);
+                    break;
+                case 1:
+                    intent = new Intent(context,BranchActivity.class);
+                    break;
+                case 2:
+                    intent = new Intent(context,HeartActivity.class);
+                    break;
+                case 3:
+                    intent = new Intent(context,StoreActivity.class);
+                    break;
+                case 4:
+                    intent = new Intent(context,SponsorActivity.class);
+                    break;
+                case 5:
+                    intent = new Intent(context,WebActivity.class);
+                    break;
+                case 6:
+                    intent = new Intent(context,OptionActivity.class);
+                    break;
+                case 7:
+                    intent = new Intent(context,QuestionActivity.class);
+                    break;
+                case 8:
+                    intent = new Intent(context,SuggestActivity.class);
+                    break;
+                default:
+                    intent = null;
+            }
+            context.startActivity(intent);
         }
     }
 
@@ -51,16 +92,16 @@ public class NavIconAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position){
         NavViewHolder navViewHolder = (NavViewHolder) holder;
 
         navViewHolder.navIconPic.setImageResource(NavIconArrayList.get(position).drawableId);
         navViewHolder.navIconName.setText(NavIconArrayList.get(position).viewname);
-
     }
 
     @Override
     public int getItemCount() {
         return NavIconArrayList.size();
     }
+
 }
